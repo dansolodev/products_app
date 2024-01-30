7863580cf450d8082be63587bac6cf9da675456e
dansolodev <dansolo.cc@gmail.com>
2024-01-29 22:59:28 -0600

    start login form

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -27,12 +27,64 @@ class LoginScreen extends StatelessWidget {
                     const SizedBox(
                       height: 30,
                     ),
-                    Text('Form'),
+                    _LoginForm()
                   ],
                 ),
               ),
+              const SizedBox(
+                height: 50,
+              ),
+              const Text(
+                'Crear una nueva cuenta',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  const _LoginForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.deepPurple,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.deepPurple,
+                    width: 2,
+                  ),
+                ),
+                hintText: 'sample@mail.com',
+                labelText: 'Email',
+                labelStyle: TextStyle(
+                  color: Colors.grey,
+                ),
+                prefixIcon: Icon(
+                  Icons.alternate_email_sharp,
+                  color: Colors.deepPurple,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
