4ab79e5429a78ab6b2a815c8a25d046e6381de12
dansolodev <dansolo.cc@gmail.com>
2024-02-16 16:23:24 -0600

    Signup screen

---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'package:products_app/providers/login_form_provider.dart';
+import 'package:provider/provider.dart';
+
+import 'package:products_app/ui/input_decorations.dart';
+import 'package:products_app/widgets/widgets.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AuthBackground(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 250,
+              ),
+              CardContainer(
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Signup',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    ChangeNotifierProvider(
+                      create: (_) => LoginFormProvider(),
+                      child: const _SignupForm(),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              TextButton(
+                onPressed: () =>
+                    Navigator.pushReplacementNamed(context, 'login'),
+                style: ButtonStyle(
+                  overlayColor:
+                      MaterialStateProperty.all(Colors.indigo.withOpacity(0.1)),
+                  shape: MaterialStateProperty.all(const StadiumBorder()),
+                ),
+                child: const Text(
+                  '¿Ya tienes una cuenta?',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black87,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _SignupForm extends StatelessWidget {
+  const _SignupForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final loginForm = Provider.of<LoginFormProvider>(context);
+
+    return Form(
+      key: loginForm.formKey,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      child: Column(
+        children: [
+          TextFormField(
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecorations.authInputDecoration(
+                hintText: 'email@sample.com',
+                labelText: 'Email',
+                prefixIcon: Icons.alternate_email_sharp),
+            onChanged: (value) {
+              loginForm.email = value;
+            },
+            validator: (value) {
+              String pattern =
+                  r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+              RegExp regExp = RegExp(pattern);
+              return regExp.hasMatch(value ?? '')
+                  ? null
+                  : 'El valor ingresado no luce como un correo';
+            },
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          TextFormField(
+            autocorrect: false,
+            obscureText: true,
+            keyboardType: TextInputType.text,
+            decoration: InputDecorations.authInputDecoration(
+                hintText: '*****',
+                labelText: 'Password',
+                prefixIcon: Icons.lock),
+            onChanged: (value) {
+              loginForm.password = value;
+            },
+            validator: (value) {
+              if (value != null && value.length >= 6) return null;
+              return 'La contraseña debe de ser de 6 caracteres';
+            },
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          MaterialButton(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            disabledColor: Colors.grey,
+            elevation: 0,
+            color: Colors.deepPurple,
+            onPressed: loginForm.isLoading
+                ? null
+                : () async {
+                    FocusScope.of(context).unfocus();
+                    if (!loginForm.isValidForm()) return;
+                    loginForm.isLoading = true;
+                    await Future.delayed(const Duration(seconds: 3));
+                    loginForm.isLoading = false;
+                    Navigator.pushReplacementNamed(context, 'home');
+                  },
+            child: Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 80,
+                vertical: 15,
+              ),
+              child: Text(
+                loginForm.isLoading ? 'Espere..' : 'Login',
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
